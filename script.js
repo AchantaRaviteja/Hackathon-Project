7bd1e0bffb106ffd13ee4be4dcf87ffa06500ba4
AchantaRaviteja <140535001+AchantaRaviteja@users.noreply.github.com>
2024-02-07 20:53:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let tabButtons = document.querySelectorAll(".buttonContainer button");
 let tabPanels = document.querySelectorAll(".tabPanel");
+let FlashCards = document.querySelectorAll(".FlashCardList");
+let Flashes = document.querySelectorAll(".FlashCardContainer #Flash");
+let Interact = document.querySelectorAll("#Interact");
 
 function showPanel(idx){
     tabPanels.forEach(function(node){
@@ -8,3 +11,25 @@ function showPanel(idx){
     tabPanels[idx].style.display = "block";
 };
 showPanel(0);
+
+function showFlashCard(val){
+Flashes.forEach(function(node){
+        node.style.display = "none";
+    });
+    Flashes[val].style.display = "block";
+    FlashCards[0].style.display = "none";
+    for(let i=0;i<2;i++){
+        Interact[i].style.display = "block";
+    }
+   
+}
+
+function Reset(){
+    Flashes.forEach(function(node){
+        node.style.display = "none";
+    });
+    FlashCards[0].style.display = "flex";
+    for(let i=0;i<2;i++){
+        Interact[i].style.display = "none";
+    }
+}
